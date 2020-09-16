19fb428d9d17f69bc23ce0b29c6f4911a8cf5983
Shawn Lim <shawnlim89@live.com>
2020-09-16 21:28:34 +0800

    Bind to 0.0.0.0

--- app.py
+++ app.py
@@ -83,6 +83,6 @@ APP.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
     try:
-        web.run_app(APP, host="localhost", port=CONFIG.PORT)
+        web.run_app(APP, host="0.0.0.0", port=CONFIG.PORT)
     except Exception as error:
         raise error

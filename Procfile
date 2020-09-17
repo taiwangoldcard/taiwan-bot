a0a93969767f98816534ea44341fa86f5753103b
Shawn Lim <shawnlim89@live.com>
2020-09-17 22:49:43 +0800

    Fix ports

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app:app
+web: uvicorn app:app --host=0.0.0.0 --port=${PORT:-8000}
--- config.py
+++ config.py
@@ -6,7 +6,5 @@ import os
 
 class DefaultConfig:
     """ Bot Configuration """
-
-    PORT = os.environ.get("PORT", 3978)
     APP_ID = os.environ.get("MICROSOFT_APP_ID", "")
     APP_PASSWORD = os.environ.get("MICROSOFT_APP_PASSWORD", "")

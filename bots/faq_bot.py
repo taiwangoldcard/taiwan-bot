a305bce2d25be11eefe400ee165f94cdcaf67f44
Jonathan <john@Jonathans-MacBook-Pro.local>
2020-09-20 17:13:42 +0800

    Added FAQBot which uses the QA model to answer messages. Set the default bot to FAQBot.

---
+++ bots/faq_bot.py
@@ -0,0 +1,22 @@
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from botbuilder.schema import ChannelAccount
+
+
+class FAQBot(ActivityHandler):
+    """A model to find the most relevant answers for specific questions."""
+
+    def __init__(self, qa_model):
+        self.qa_model = qa_model
+    
+    async def on_members_added_activity(
+        self, members_added: [ChannelAccount], turn_context: TurnContext
+    ):
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Hello and welcome!")
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        return await turn_context.send_activity(
+            MessageFactory.text(
+                f"I'm echoing you: {self.qa_model.find_best_answer(turn_context.activity.text)}")
+        )

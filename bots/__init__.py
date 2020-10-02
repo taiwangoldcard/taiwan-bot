1bda32449c936c2ebca72693bb31ac2b65a2ce7a
Shawn Lim <shawnlim89@live.com>
2020-10-02 23:40:05 +0800

    Persist context for gold card state

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,4 +1,5 @@
 from .echo_bot import EchoBot
 from .faq_bot import FAQBot
+from .conversation_data import ConversationData
 
-__all__ = ["EchoBot", "FAQBot"]
+__all__ = ["EchoBot", "FAQBot", "ConversationData"]
--- bots/faq_bot.py
+++ bots/faq_bot.py
@@ -1,22 +1,40 @@
-from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
+from taiwan_bot_sheet import SpreadsheetContext
+from botbuilder.core import ActivityHandler, MessageFactory, TurnContext, ConversationState
 from botbuilder.schema import ChannelAccount
+from .conversation_data import ConversationData
+import re
+
 
 class FAQBot(ActivityHandler):
     """A model to find the most relevant answers for specific questions."""
 
-    def __init__(self, qa_model):
+    def __init__(self, qa_model, conversation_state: ConversationState):
         self.qa_model = qa_model
-    
-    async def on_members_added_activity(
-        self, members_added: [ChannelAccount], turn_context: TurnContext
-    ):
-        for member in members_added:
-            if member.id != turn_context.activity.recipient.id:
-                await turn_context.send_activity("Hello and welcome!")
+        self.conversation_state = conversation_state
+        self.conversation_data_accessor = self.conversation_state.create_property(
+            "ConversationData")
+        self.regex = re.compile("gold card", re.IGNORECASE)
 
     async def on_message_activity(self, turn_context: TurnContext):
+        conversation_data = await self.conversation_data_accessor.get(
+            turn_context, ConversationData
+        )
+        conversation_data.timestamp = turn_context.activity.timestamp
+
+        conversation_data.channel_id = turn_context.activity.channel_id
+        conversation_data.recipient_id = turn_context.activity.recipient.id
+
+        if re.search(self.regex, turn_context.activity.text) is not None:
+            conversation_data.context = SpreadsheetContext.GOLDCARD
+
+        # TODO: find_best_answer should be called on different set based on context
         return await turn_context.send_activity(
             MessageFactory.text(
                 self.qa_model.find_best_answer(turn_context.activity.text)
             )
         )
+
+    async def on_turn(self, turn_context: TurnContext):
+        await super().on_turn(turn_context)
+
+        await self.conversation_state.save_changes(turn_context)

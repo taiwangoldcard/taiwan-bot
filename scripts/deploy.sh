515fa76a39b7aac9acd2df7cd8a0b15a1c1d7829
Geoffrey Lee <geofflee25@gmail.com>
2020-11-28 00:16:54 +0800

    Update setup/deploy scripts for new infra

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@ echo "Deploying..."
 ssh taiwan-bot /bin/sh << EOF
 sudo supervisorctl stop app
 cd ~/app
-. env/bin/activate
-git fetch --all
-git reset --hard origin/master
+. .venv/bin/activate
+sudo git fetch --all
+sudo git reset --hard origin/master
 pip install --no-cache-dir -r requirements.txt
 sudo supervisorctl start app
 EOF
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # environment assumes an ubuntu 20.04, please setup your ~/.ssh/config first
 ssh taiwan-bot /bin/sh << EOF
-pip3 install virtualenv
-git clone https://github.com/taiwangoldcard/taiwan-bot.git app
+sudo pip3 install virtualenv
+sudo git clone https://github.com/taiwangoldcard/taiwan-bot.git app
 cd app
-~/.local/bin/virtualenv -p /usr/bin/python3 env
-. env/bin/activate
+virtualenv .venv
+. .venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
 deactivate
 EOF
